bf9d6422ae3663470bf9f1a29796059ef0c36d49
teunpe <teun.peeters99@gmail.com>
2024-10-29 13:05:24 +0100

    update cluster run

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --account=cseduproject
+#SBATCH --partition=csedu
+#SBATCH --qos=csedu-normal
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4G
+#SBATCH --gres=gpu:1
+#SBATCH --time=1:00:00
+#SBATCH --output=myjob-%j.out
+#SBATCH --error=myjob-%j.err
+
 source //vol/csedu-nobackup/project/tpeeters/venv/bin/activate
 python main.py --lr 0.001 --dataname mnist --n_clients 10 --iid --n_epochs 10
 python synthetic_data.py --dataname mnist --n_clients 10 --iid
