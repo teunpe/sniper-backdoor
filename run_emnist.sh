67f6de466df60a7c67d2c7e28e232b4c6aa9e46d
teunpe <teun.peeters99@gmail.com>
2024-10-29 16:03:00 +0100

    update file names and location defaults

--- run_emnist.sh
+++ run_emnist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 conda activate intern
-python main.py --lr 0.001 --dataname emnist --n_clients 10 --iid --n_epochs 10 --datadir '/media/teun/ChonkyBoi/internship/data'
-python synthetic_data.py --dataname emnist --n_clients 10 --iid --datadir '/media/teun/ChonkyBoi/internship/data''
-python shadow_network.py --dataname emnist --fake_dir ./results/fake_datasets_mnist --n_epochs 10 --iid --datadir '/media/teun/ChonkyBoi/internship/data''
+python main.py --lr 0.001 --dataname emnist --n_clients 10 --iid --n_epochs 10
+python synthetic_data.py --dataname emnist --n_clients 10 --iid 
+python shadow_network.py --dataname emnist --n_epochs 10 --iid
 python client_identification.py --epochs 10 --n_clients 10 --dataname emnist
-python personalize_model.py --epochs 10 --n_clients 10 --dataname emnist --iid --datadir '/media/teun/ChonkyBoi/internship/data''
+python personalize_model.py --epochs 10 --n_clients 10 --dataname emnist --iid
