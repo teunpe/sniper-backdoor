0d81ae8c22d170ddc52afd12e77652fa4ecd33b1
teunpe <teun.peeters99@gmail.com>
2024-12-13 15:09:54 +0100

    reset backdoor args

--- backdoor_cifar.py
+++ backdoor_cifar.py
@@ -18,29 +18,29 @@ from trojanvision.attacks import BackdoorAttack
 import trojanvision.configs
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument('--dir', type=str, default='./results', help='directory')
-    # trojanvision.environ.add_argument(parser)
-    # trojanvision.datasets.add_argument(parser)
-    # trojanvision.models.add_argument(parser)
-    # trojanvision.trainer.add_argument(parser)
-    # trojanvision.marks.add_argument(parser)
-    # trojanvision.attacks.add_argument(parser)
-    # kwargs = vars(parser.parse_args())
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=str, default='./results', help='directory')
+    trojanvision.environ.add_argument(parser)
+    trojanvision.datasets.add_argument(parser)
+    trojanvision.models.add_argument(parser)
+    trojanvision.trainer.add_argument(parser)
+    trojanvision.marks.add_argument(parser)
+    trojanvision.attacks.add_argument(parser)
+    kwargs = vars(parser.parse_args())
+    args = parser.parse_args()
     
     dataset = CIFAR100(root='//vol/csedu-nobackup/project/tpeeters/data', train=True,
                         download=True)
-    # env = trojanvision.environ.create(**kwargs)
-    # dataset = trojanvision.datasets.create(**kwargs)
-    # model = trojanvision.models.create(model_name='vgg11_bn', model='vgg11_bn', dataset_name='cifar100', dataset=dataset)
-    # # model = build_model(100, 'cifar100')
-    # server_model = torch.load(os.path.join(args.dir, 'cifar100_iid_True_server_results.pt'))['model']
-    # model.load_state_dict(server_model)
-    # trainer = trojanvision.trainer.create(dataset=dataset, model=model, **kwargs)
-    # mark = trojanvision.marks.create(dataset=dataset, **kwargs)
-    # attack: BackdoorAttack = trojanvision.attacks.create(dataset=dataset, model=model, mark=mark, **kwargs)
+    env = trojanvision.environ.create(**kwargs)
+    dataset = trojanvision.datasets.create(**kwargs)
+    model = trojanvision.models.create(model_name='vgg11_bn', model='vgg11_bn', dataset_name='cifar100', dataset=dataset)
+    # model = build_model(100, 'cifar100')
+    server_model = torch.load(os.path.join(args.dir, 'cifar100_iid_True_server_results.pt'))['model']
+    model.load_state_dict(server_model)
+    trainer = trojanvision.trainer.create(dataset=dataset, model=model, **kwargs)
+    mark = trojanvision.marks.create(dataset=dataset, **kwargs)
+    attack: BackdoorAttack = trojanvision.attacks.create(dataset=dataset, model=model, mark=mark, **kwargs)
 
-    # if env['verbose']:
-    #     trojanvision.summary(env=env, dataset=dataset, model=model, mark=mark, trainer=trainer, attack=attack)
-    # attack.attack(**trainer)
+    if env['verbose']:
+        trojanvision.summary(env=env, dataset=dataset, model=model, mark=mark, trainer=trainer, attack=attack)
+    attack.attack(**trainer)
